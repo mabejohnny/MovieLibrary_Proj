a6866d634869218b54c8d6c1699f6b36ef60faaf
Josh Grimaldi <josh.grimaldi@outlook.com>
2020-10-30 10:56:57 -0400

    formated our table on the html side, put together ajax 'get' and going to test now

--- Client/app.js
+++ Client/app.js
@@ -7,12 +7,13 @@
         };
 
         $.ajax({
-            url: 'http://localhost:44325/api/movie',
+            url: 'https://localhost:44325/api/movie',
             dataType: 'json',
             type: 'post',
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
+                console.log("success");
                 $('#response pre').html( data );
             },
             error: function( jqXhr, textStatus, errorThrown ){
@@ -23,6 +24,41 @@
         e.preventDefault();
     }
 
-    $('#new-form').submit( processForm );
+    $('#my-form').submit( processForm );
+
+
+    function getMovies( e ){
+
+    };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            success: function(textStatus, jQxhr ){
+                console.log("success");
+                $('#my-table tableBody').html( data );
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })(jQuery);
 
