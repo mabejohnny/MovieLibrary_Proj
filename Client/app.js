23669958ea26111ec60d96112b410794f916c96b
Janelle Johnson <janelleblevins@gmail.com>
2020-11-02 12:48:25 -0600

    Created search by parameters for movies

--- Client/app.js
+++ Client/app.js
@@ -67,6 +67,66 @@ function editMovie(id){
 
 });
 }
+//another seach buttom for single "trait"
+//
+function titleSearch() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("titleInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("allMoviesTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+  function directorSearch() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("directorInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("allMoviesTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+  function genreSearch() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("genreInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("allMoviesTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[2];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+
+
+
 function getMovies(){
     
     $.ajax({
